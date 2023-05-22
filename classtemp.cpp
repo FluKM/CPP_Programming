c20d296992344c19a60b9167b948c41eb70f2758
FLUK <121596131+FluKM@users.noreply.github.com>
2023-05-22 11:54:33 +0530

    Add files via upload

---
+++ classtemp.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+template<class T>
+class show
+{
+    T a,b;
+    public:
+    show(T p,T q)
+    {
+        a=p;
+        b=q;
+    }
+    void display()
+    {
+        cout<<"A="<<a<<" "<<"B="<<b<<endl;
+    }
+};
+
+int main()
+{
+    show <int> ob1(10,20);
+    ob1.display();
+    show <float> ob2(10.5,20.5);
+    ob2.display();
+    show <char> ob3('x','y');
+    ob3.display();
+    return(0);
+}
